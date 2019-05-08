da5317d8a58fc90edfbe009129e1b3d0cc372042
ttp <527813949@qq.com>
2019-05-08 21:02:32 +0800

    5-8

---
+++ src/test/T.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author XRZ
+ * @date 2019/5/8
+ * @Description :
+ */
+public class T {
+    public static void main(String[] args) {
+        System.out.println("halo wold");
+    }
+}
